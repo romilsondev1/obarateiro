b9f5b6086d912587d97d0425944911b841dabb60
romilsondev1 <74967899+romilsondev1@users.noreply.github.com>
2024-03-20 11:09:43 -0300

    return to use requests

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import re
@@ -13,38 +14,12 @@ print('___api started____')
 @app.route('/api/consultproduct', methods=['GET'])
 def consulting_product():
     try:
-        #started navegation
-        chrome_options =  Options()
-        #desabled open to navigation
-        chrome_options.add_argument('--headless=new')
-        #configure service
-        chrome_service = webdriver.ChromeService(executable_path='/tmp/chromedriver',log_output='/tmp/chromedriver.log')
-        #configure driver
-        driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-
         base_url = 'https://www.kabum.com.br/espaco-gamer/cadeiras-gamer?page_number=1&page_size=20&facet_filters=&sort=price'
 
-        #start navegation
-        driver.get(base_url)
-
-        page = driver.page_source.encode('utf-8')
-
-        soup = BeautifulSoup(page , 'html.parser')
-
-        lista_produtos = []
-    
-        produtos = soup.find_all('div', class_=re.compile('productCard'))
-        for produto in produtos:
-                marca = produto.find('span', class_=re.compile('nameCard')).get_text().strip()
-                preco = produto.find('span', class_=re.compile('priceCard')).get_text().strip()
-
-                lista_produtos.append({'marca': marca, 'preco': preco})
-    
-
-        '''
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
 
-        site = requests.get(url, headers=headers)
+        session = HTMLSession()
+        site = session.get(base_url, headers=headers)
         soup = BeautifulSoup(site.content, 'html.parser')
 
         produtos = soup.find_all('div', class_=re.compile('productCard'))
@@ -55,7 +30,7 @@ def consulting_product():
                 print(marca)
                 print(preco)
         
-        
+        '''
         qtd_items = soup.find('div', id="listingCount").get_text().strip()
         index = qtd_items.find(' ')
         qtd = qtd_items[:index]
@@ -75,15 +50,12 @@ def consulting_product():
 
                 print(marca)
         '''
-        driver.quit()
 
         return jsonify({
             'status': 200,
             'message': 'final da chamada api',
-            'data': lista_produtos
         })
     except error as error:
-         driver.quit() 
          print(error)
 
 if __name__ == '__main__':
